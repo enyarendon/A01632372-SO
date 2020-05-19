ec1ad4686723b783b1eb5d98659f42afc545253c
enyarendon <enyarendon@gmail.com>
2020-05-19 23:37:00 +0000

    Práctica 11

---
+++ Práctica11/copy.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFSIZE 512
+
+int main (int argc, char** argv)
+{
+    char *srcFilename = argv[1];
+    char *dstFileName = argv[2];
+    int from, to, nr, nw, n;
+    char buf[BUFSIZE];
+
+    if ((from=open(srcFilename, O_RDONLY)) < 0) {
+                printf("Error opening source file");
+                exit(1);
+        }
+        if ((to=creat(dstFileName, 0666)) < 0) {
+                perror("Error creating destination file");
+                exit(2);
+
+        }
+        while((nr=read(from, buf, sizeof( buf))) != 0) {
+            if (nr < 0) {
+            perror("Error reading source file");
+            exit(3);
+            }
+            nw=0;
+            do {
+            if ((n=write(to, &buf[nw], nr-nw)) < 0) {
+                perror("Error writing destination file");
+                exit(4);
+            }
+
+            nw += n;
+            } while (nw < nr);
+        }
+        close(from); close(to);
+}
